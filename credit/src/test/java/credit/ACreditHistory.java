badc47530c32f55df7b8a25105ffeb772273ead1
Jamal Hansen <jamal.hansen@gmail.com>
2025-01-20 20:49:13 -0600

    Finished Chapter 1

--- credit/src/test/java/credit/ACreditHistory.java
+++ credit/src/test/java/credit/ACreditHistory.java
@@ -3,12 +3,31 @@ package credit;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class ACreditHistory {
+  CreditHistory creditHistory;
+
+  @BeforeEach
+  void setup() {
+    creditHistory = new CreditHistory();
+  }
 
   @Test
   void withNoCreditRatingsHas0Mean() {
-    var creditHistory = new CreditHistory();
     var result = creditHistory.arithmaticMean();
+
     assertEquals(0, result);
+  }
+
+  @Test
+  void withTMultipleCreditRatingsHasTheMean() {
+    creditHistory.add(new CreditRating(780));
+    creditHistory.add(new CreditRating(800));
+    creditHistory.add(new CreditRating(820));
+
+    var result = creditHistory.arithmaticMean();
+
+    assertEquals(800, result);
   }
 }
